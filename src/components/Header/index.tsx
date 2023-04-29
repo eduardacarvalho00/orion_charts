848ec8f6d34478386a014f50270fbb3f0d546022
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-04-28 21:20:31 -0300

    feat: creating header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-children-prop */
+import { Icon, SearchIcon } from '@chakra-ui/icons';
+import {
+  Image, Flex, Input, InputGroup, InputRightElement, Grid, GridItem, Link,
+} from '@chakra-ui/react';
+import { MdOutlinePerson } from 'react-icons/md';
+import { FiSettings } from 'react-icons/fi';
+import logo from '../../assets/logo.png';
+
+export function Header() {
+  return (
+    <Flex as="header" h="60px" bg="purple.900" align="center" justify="space-around">
+        <Image src={logo} />
+        <InputGroup bg="gray.900" borderRadius="8px" w="520px" h="32px" borderColor="transparent">
+          <Input borderRadius="8px" h="32px" />
+          <InputRightElement children={<SearchIcon color="purple.700" boxSize={5} />} />
+        </InputGroup>
+
+        <Grid templateColumns="repeat(4, 1fr)" gap="80px">
+          <GridItem>
+            <Link color="purple.500" fontSize={16}>Statistics</Link>
+          </GridItem>
+          <GridItem>
+            <Link color="purple.500" fontSize={16}>Overview</Link>
+          </GridItem>
+          <GridItem>
+            <Link color="purple.500" fontSize={16}>Dashboard</Link>
+          </GridItem>
+          <GridItem>
+            <Link color="purple.500" fontSize={16}>Analytics</Link>
+          </GridItem>
+        </Grid>
+
+        <Flex flex="row">
+          <Icon as={MdOutlinePerson} boxSize={8} color={'purple.500'} mr="18px" />
+          <Icon as={FiSettings} boxSize={7} color={'purple.500'} />
+        </Flex>
+      </Flex>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,61 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      900: '#05050F',
+      800: '#111111',
+      700: '#828282',
+      600: '#CFCFCF',
+      500: '#BDBDBD',
+      300: '#E0E0E0',
+      200: '#F2F2F2', 
+      100: '#F8F9FB',
+      50: '#FFFFFF', 
+    },
+    blue: {
+      800: '#0D0D2B',
+      700: '#25245D',
+      600: '#35068C',
+      500: '#023AFF',
+      400: '#3671E9',
+      300: '#1181E8',
+      200: 'linear-gradient(270deg, #4DFFDF 0%, #4DA1FF 96.35%)',
+    },
+    purple: {
+      900: '#191932',
+      800: '#491F98',
+      600: '#991BFA',
+      500: '#6A6A9F',
+      200: 'linear-gradient(270.15deg, #7D40FF 19.44%, #02A4FF 81.9%)',
+      100: 'linear-gradient(269.95deg, #FF2397 0.02%, #7517F8 97.45%)',
+    },
+    yellow: {
+      500: '#FFA63F',
+    },
+    green: {
+      500: '#5EFF5A',
+      300: '#59C19C',
+      200: '#01F1E3',
+    },
+    red: {
+      900: '#FF2D2E',
+    },
+  },
+  fonts: {
+    heading: 'Open Sans',
+    body: 'Open Sans',
+
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50',
+      },
+      '*': {
+        scrollBehavior: 'smooth',
+      },
+    },
+  },
+});
